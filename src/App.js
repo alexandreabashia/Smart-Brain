784d96bb7a3c24d21b1432b1cbd63d03ce175a09
Alexandre <alexandreabashia@gmail.com>
2020-12-11 03:53:32 +0400

    update 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/Imagelinkform/ImageLinkForm';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm /> */}
+      {/* </Rank> */}
+      <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
   );
 }
 
 export default App;
+
+
+
+// 27:00 i stopped 
